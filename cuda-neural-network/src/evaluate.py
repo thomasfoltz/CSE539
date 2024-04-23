2e75c022d2b501fe9e14815a247c55d2ab10035d
thomasfoltz <thomasfoltz42@gmail.com>
2024-04-23 10:15:51 -0400

    modified evaluation script

--- cuda-neural-network/src/evaluate.py
+++ cuda-neural-network/src/evaluate.py
@@ -1,29 +1,28 @@
+import csv
+
 def calculate_average_duration(data):
   average_durations = []
 
-  for i in range(0, len(data), 5):
-    neural_network_data = data[i:i+5]
+  for i in range(0, len(data), 4):
+    neural_network_data = data[i:i+4]
 
     sum_duration = 0
     for row in neural_network_data:
-      duration = float(row[2])
+      duration_str = row["Duration"]
+      duration = float(duration_str.split()[0])
       sum_duration += duration
-    average_duration = sum_duration / len(neural_network_data)
 
+    average_duration = sum_duration / len(neural_network_data)
     average_durations.append(average_duration)
 
-  return average_durations
+  total_average_duration = sum(average_durations) / len(average_durations)
+  return total_average_duration
 
-data = [
-    ["linearLayerForward(float, float, float *....", 0.2948185, 2.304, "GPU 0  Stream 16"],
-    ["reluActivation Forward(float, float, int....", 0.2948235, 2.176, "GPU 0  Stream 16"],
-    ["linearLayerForward(float, float, float *....", 0.2948275, 3.329, "GPU 0  Stream 16"],
-    ["reluActivation Forward(float, float, int,...", 0.2948315, 1.824, "GPU 0  Stream 16"],
-    ["sigmoidActivation Forward(float, float....", 0.2948355, 1.920, "GPU 0  Stream 16"],
-    ["linearLayerForward(float, float, float *....", 0.2948945, 2.208, "GPU 0  Stream 16"],
-]
+data = []
+with open('./stream_4_batch_4(Sheet1).csv', 'r') as file:
+  reader = csv.DictReader(file)
+  for row in reader:
+    data.append(row)
 
-average_durations = calculate_average_duration(data)
-
-for i, avg_duration in enumerate(average_durations):
-  print(f"Average duration for neural network {i+1}: {avg_duration:.6f} μs")
+average_duration = calculate_average_duration(data)
+print(f"Average duration for all neural networks: {average_duration:.6f} μs")
